2ac8832b1fe304bff8d9957458b59e540ad4c74a
Muhammad Khoirul Huda <114276467+k1zuko@users.noreply.github.com>
2025-09-02 15:31:02 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-};
-
-module.exports = {
   images: {
     unoptimized: true, // Nonaktifkan optimisasi gambar global
     domains: [],
     disableStaticImages: true // Memaksa penggunaan import untuk gambar statis
   }
-}
+};
+
+// module.exports = {
+// }
 
 export default nextConfig;
